32eed5caf4d997e6eadde88bfd569e8910850fb8
ABHI <abhisekprusty012@gmail.com>
2026-06-16 19:52:44 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div id="parent">
+        <h1 id="child">Hello</h1>
+        <h2 id="child">Hello</h2>
+      </div>
+      <div id="chacha">
+        <h3>Hola</h3>
+      </div>
+    </>
+  );
+};
+
+export default App;
