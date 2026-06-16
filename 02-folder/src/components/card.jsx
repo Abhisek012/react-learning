67e9c5e9701f35ac69cbfad6bc1099c92073b0de
ABHI <abhisekprusty012@gmail.com>
2026-06-16 20:35:11 +0530

    02-folder - component react

---
+++ 02-folder/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Hello everyone I'm Abhisek</h1>
+        <h3>I am backend engineer.</h3>
+      </div>
+    </div>
+  );
+};
+
+export default card;
