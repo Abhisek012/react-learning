aa0b9779c5906b4c4ca54c19aec993aa3523379d
ABHI <abhisekprusty012@gmail.com>
2026-06-18 21:21:02 +0530

    changes

---
+++ 07-ui-project/src/components/section1/LeftContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LeftContent = () => {
+  return (
+    <div className='h-full w-1/3 bg-blue-600'>
+        <div>
+            Prospective <br /> <span>Customer</span> <br /> Segmentation
+        </div>
+    </div>
+  )
+}
+
+export default LeftContent
